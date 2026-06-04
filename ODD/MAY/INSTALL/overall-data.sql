5f791004cb8d2e34c5c78e926ad3f34e34517b4f
Nirav Chauhan <121652960+NiravChauhan45@users.noreply.github.com>
2026-06-04 11:12:04 +0530

    Add files via upload

---
+++ ODD/MAY/INSTALL/overall-data.sql
@@ -0,0 +1,14 @@
+SELECT
+  active_users,
+  free_users,
+  paid_users,
+  app_status,
+  DATE_FORMAT(last_month_date, '%Y-%m') AS MONTH
+FROM
+  monthly_allover_data
+WHERE
+  DATE_FORMAT(last_month_date, '%Y-%m') = '2026-03'
+ORDER BY
+  id DESC
+LIMIT
+  1;
